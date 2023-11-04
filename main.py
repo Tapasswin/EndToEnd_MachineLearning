138e2a53bf35c3b735f9517e3fa750e95bb4df75
Tapasswin <manchala.tapasswin955@gmail.com>
2023-11-04 22:29:29 +0530

    Model Training

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from mlproject.pipeline.stage_01_data_Ingestion import *
 from mlproject.pipeline.stage_02_data_validation import *
 from mlproject.pipeline.stage_03_data_transformation import *
-
+from mlproject.pipeline.stage_04_model_trainer import *
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -33,6 +33,18 @@ if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME = "Model Trainer stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
