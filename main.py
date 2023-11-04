ae2765b418ebfc88fc94cf5c2cdf2713bfd0883a
Tapasswin <manchala.tapasswin955@gmail.com>
2023-11-04 21:39:21 +0530

    Data Transformation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from mlproject.pipeline.stage_01_data_Ingestion import *
 from mlproject.pipeline.stage_02_data_validation import *
+from mlproject.pipeline.stage_03_data_transformation import *
+
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -19,6 +21,18 @@ if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME = "Data Transformation stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataTransformationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
