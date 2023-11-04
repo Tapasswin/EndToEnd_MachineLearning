85002d72b5f733fdd4e4c8b76d20c1a6b40bf89a
Tapasswin <manchala.tapasswin955@gmail.com>
2023-11-04 23:30:58 +0530

    model evaluation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlproject.pipeline.stage_01_data_Ingestion import *
 from mlproject.pipeline.stage_02_data_validation import *
 from mlproject.pipeline.stage_03_data_transformation import *
 from mlproject.pipeline.stage_04_model_trainer import *
+from mlproject.pipeline.stage_05_model_evaluation import *
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -48,5 +49,15 @@ if __name__ == '__main__':
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
         logger.exception(e)
         raise e
